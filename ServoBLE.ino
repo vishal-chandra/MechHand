9869989caf52fce156fe66d9083d9257e1e6eabb
Vishal Chandra <vishalchandra724@gmail.com>
2019-06-03 22:34:51 -0400

    add .gitignore and other minor changes

--- ServoBLE.ino
+++ ServoBLE.ino
@@ -13,8 +13,7 @@
 #include <BLEServer.h>
 #include <ESP32Servo.h>
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
+//establishes connection to Blynk app
 char auth[] = "7e305e4259c24f6795f3a723d8657e5c";
 #define servop1 32
 #define servop2 33
@@ -27,58 +26,59 @@ Servo servo3;
 Servo servo4;
 Servo servo5;
 
-BLYNK_WRITE(V0)
-{
+//Pinky
+BLYNK_WRITE(V0){
   //get and send slider info
   int angle = param.asInt();
   servo1.write(angle);
 }
-BLYNK_WRITE(V1)
-{
+//Thumb
+BLYNK_WRITE(V1){
   //get and send slider info
   int angle = param.asInt();
   servo2.write(angle);
 }
-BLYNK_WRITE(V2)
-{
+//Ring
+BLYNK_WRITE(V2){
   //get and send slider info
   int angle = param.asInt();
   servo3.write(angle);
 }
-BLYNK_WRITE(V3)
-{
+//Index
+BLYNK_WRITE(V3){
   //get and send slider info
   int angle = param.asInt();
   servo4.write(angle);
 }
-BLYNK_WRITE(V4)
-{
+//Middle
+BLYNK_WRITE(V4){
   //get and send slider info
   int angle = param.asInt();
   servo5.write(angle);
 }
 
-void setup()
-{
+void setup(){
+  //pins
   pinMode(servop1, OUTPUT);
   pinMode(servop2, OUTPUT);
   pinMode(servop3, OUTPUT);
   pinMode(servop4, OUTPUT);
   pinMode(servop5, OUTPUT);
- 
+
+  //servos
   servo1.attach(servop1, 500, 2400);
   servo2.attach(servop2, 500, 2400); 
   servo3.attach(servop3, 500, 2400); 
   servo4.attach(servop4, 500, 2400); 
   servo5.attach(servop5, 500, 2400); 
-   
+  
+  //misc
   Serial.begin(9600);
   Serial.println("Waiting for connections...");
   Blynk.setDeviceName("Blynk");
   Blynk.begin(auth);
 }
 
-void loop()
-{
+void loop(){
   Blynk.run();
 }
